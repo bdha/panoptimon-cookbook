bde14041c1349066e0e001e161bcbeb5f4dc4f1c
Bryan Horstmann-Allen <ballen@sourcefire.com>
2013-05-14 13:08:04 -0700

    Update maintainer and bump version

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "panoptimon"
 maintainer       "Sourcefire"
-maintainer_email "mcarruthers@sourcefire.com"
+maintainer_email "sysops@sourcefire.com"
 license          "All rights reserved"
 description      "Installs/Configures panoptimon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
