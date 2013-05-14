f361a853650a10e74c87344147a228ed983442d3
Bryan Horstmann-Allen <ballen@sourcefire.com>
2013-05-14 13:13:03 -0700

    Add support stanza

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,8 @@ maintainer_email "sysops@sourcefire.com"
 license          "All rights reserved"
 description      "Installs/Configures panoptimon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
+
+%w{ centos }.each do |os|
+  supports os
+end
